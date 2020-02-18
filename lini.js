e733884db7b8abfd7b41fd9e85223462f5eee5a0
lzhan0106 <56207884+lzhan0106@users.noreply.github.com>
2020-02-17 19:07:21 -0800

    Add files via upload

--- lini.js
+++ lini.js
@@ -1,32 +1,43 @@
-import ease from 'easy-ease';
- 
-ease({
-  startValue: window.scrollY,
-  endValue: 0,
-  onStep: value => window.scroll(0, value),
-});
-
 $(document).ready(function(){
-
     const key = 'qww4Qdu3s2zsKCNl0AFU';
     let headers = new Headers();
     headers.set('x-api-key', key);
     
-    const limit = '2';
-    const url = 'https://the-one-api.herokuapp.com/v1/book' + limit;
+    const url = 'https://the-one-api.herokuapp.com/v1/book';
     
     let output = '';
+
+    let chapteroutput = '';
     
     $.getJSON(url, function(data){
-         for(i = 0; i < data.length; i++) {
-            output += '<book src="'+data[i].url+'"/>';
+         for(i = 0; i < data.docs.length; i++) {
+            output += "<h3 data-id="+data.docs[i]._id+">"+data.docs[i].name+"</h3>";
          }
         
          $('#content').append(output);
     
-         $('book').click(function(){
-            alert("You clicked!");
+         $('h3').click(function(){
+           let bookId = $(this).data("id");
+           console.log(bookId);
+            gitChapters(bookId);
         });
     });
-    
+    function gitChapters(id){
+
+      $.getJSON("https://the-one-api.herokuapp.com/v1/book/"+id+"/chapter", function(data){
+        for(i = 0; i < data.docs.length; i++) {
+           chapteroutput += "<h3>"+data.docs[i].book+"</h3>";
+
+        }
+       
+        $('#content').append(chapteroutput);
+   });
+    }
+
+
+
+
+
+
+
     });
